37385d0b5f1658e26ac57a7a07af77c8ea62d09a
Rudy Huezo <rahuezo@ucdavis.edu>
2018-03-12 16:12:50 -0700

    Finished get_actual_movers.py

--- utils/validation.py
+++ utils/validation.py
@@ -1,2 +1,6 @@
 def is_mover(locations): 
     return False if len(locations) == 1 else (len(locations) == len(set(locations)))
+
+def is_potential_mover(db, user_id, cnt=1): 
+    count = int(db.select('SELECT COUNT(user_id) FROM users_fips WHERE user_id={uid}'.format(uid=user_id)).fetchone()[0]) 
+    return count > cnt
