4c954fc793798652ac065f9fa010bd6f85b23e0a
Rudy Huezo <rahuezo@ucdavis.edu>
2018-03-13 11:36:22 -0700

    Recoded get_potential_movers.py

--- get_potential_movers.py
+++ get_potential_movers.py
@@ -1,8 +1,7 @@
-from utils.configuration import MOVERS_DB_NAME, RESULTS_DIR_PATH, MOVERS_TBNAME
+from utils.configuration import POTENTIAL_MOVERS_COLUMNS, POTENTIAL_MOVERS_TBNAME, USER_FIPS_TBNAME
 from utils.separation import chunkify
-from utils.modulars import commit_movers_chunk, create_db_and_movers_tb, get_movers, get_locations_per_db
 from utils.database import Database
-from utils.locating import get_user_location
+from utils.validation import is_potential_mover
 
 import multiprocessing as mp 
 import tkFileDialog as fd
@@ -11,80 +10,43 @@ import sys
 import os
 
 
-# 0 for raw, 1 for multiprocessing 
-
-if len(sys.argv) > 1: 
-    METHOD = int(sys.argv[1])
-else: 
-    METHOD = 0
-
-if len(sys.argv) > 2: 
-    TEST_SIZE = int(sys.argv[2])
+if len(sys.argv) > 1:
+    TEST_SIZE = int(sys.argv[1])
     if TEST_SIZE < 0: 
         TEST_SIZE = None
 else: 
     TEST_SIZE = 100
 
-METHOD_STR = 'Parallel' if METHOD else 'Raw'
-
-
-def raw_save_potential_movers(chunks, all_dbs):  
-    db_path = os.path.join(RESULTS_DIR_PATH, MOVERS_DB_NAME)
-    db = create_db_and_movers_tb(db_path)
-
-    for chunk in chunks: 
-        commit_movers_chunk(chunk, db, all_dbs)
-    db.connection.close()
-
-def parallel_save_potential_movers(chunks, all_dbs): 
-    db_path = os.path.join(RESULTS_DIR_PATH, MOVERS_DB_NAME)
-    db = create_db_and_movers_tb(db_path)
-
-    pool = mp.Pool(processes=mp.cpu_count())
-
-    processes = [pool.apply_async(commit_movers_chunk, args=(chunk, db, all_dbs)) for chunk in chunks]
-
-    for p in processes: 
-        p.get()
-    db.connection.close()
-
-
 
 if __name__ == '__main__':
-    try: 
-        dbs = fd.askopenfilenames(title='Get databases')
-        if not dbs: 
-            raise Exception('\nNo directory selected! Goodbye.\n')
+    try:
+        aggregated_data_db = fd.askopenfilename(title='Choose database with aggregated data')
+
+        if not aggregated_data_db:
+            raise Exception('\nNo database selected! Goodbye.\n')
     except Exception as e: 
         print e
         sys.exit()
 
     s = time.time()
 
-    for db_file in dbs: 
-        db = Database(db_file)
-        unique_users = [user for user in db.select('SELECT user_id FROM users')]
+    aggregated_db = Database(aggregated_data_db)
+    potential_movers_tb = aggregated_db.create_table(POTENTIAL_MOVERS_TBNAME + '_2', POTENTIAL_MOVERS_COLUMNS)
 
-        pool = mp.Pool(processes=mp.cpu_count())
+    unique_users = [user[0] for user in aggregated_db.select('SELECT DISTINCT user_id FROM {}'.format(USER_FIPS_TBNAME))][:TEST_SIZE]
 
-        processes = [pool.apply_async(get_user_location, args=(uid, db_file)) for uid in unique_users]
-        
-        movers_tb = db.create_table(MOVERS_TBNAME, 'user_id INT, tweet_location TEXT')
+    user_chunks = chunkify(unique_users, n=10000)
 
-        db.cursor.execute('BEGIN')
+    for user_chunk in user_chunks:
 
-        for p in processes: 
-            db.insert('INSERT INTO {tbn} VALUES(?, ?)'.format(tbn=movers_tb), p.get())
-        
-        db.connection.commit()
-        db.connection.close()
+        aggregated_db.cursor.execute('BEGIN')
 
-        # get_locations_per_db(db, unique_users)
+        for user in user_chunk:
+            if is_potential_mover(aggregated_db, user):
+                aggregated_db.insert('INSERT INTO {tbn} VALUES(?)'.format(tbn=potential_movers_tb), (user,))
 
-    # if METHOD: 
-    #     parallel_save_potential_movers(chunked_users, dbs)
-    # else:
-    #     raw_save_potential_movers(chunked_users, dbs)
+        aggregated_db.connection.commit()
+
+    aggregated_db.connection.close()
 
     print '\nElapsed Time: {}s\n'.format(round(time.time() - s, 2))
-    print 'Method: {}, Size: {}\n'.format(METHOD_STR, TEST_SIZE)
